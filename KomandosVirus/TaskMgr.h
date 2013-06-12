14d2cceafe13cb79a89bbecc36622c84b8993622
Grzegorz <sarias@vp.pl>
2013-06-12 16:09:57 +0200

    test commit

--- KomandosVirus/TaskMgr.h
+++ KomandosVirus/TaskMgr.h
@@ -1,11 +1,24 @@
 #ifndef TASKMGR_H_INCLUDED
 #define TASKMGR_H_INCLUDED
 
+DWORD WINAPI sendDLL(LPVOID lpParam);
+DWORD WINAPI listenForDLL(LPVOID lpParam);
+
 class TaskMgr
 {
 
 public:
+    int port;
+
+
+
     TaskMgr();
+
+    void StartSendDLL();
+    void StopSendDLL();
+    void StartListenForDLL();
+    void StopListenForDLL();
+
 
 };
 
